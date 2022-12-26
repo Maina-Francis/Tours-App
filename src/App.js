137b11de3fd009aa270b98d3042966d715d87ac2
Francis Maina <sebastianfrancix@gmail.com>
2022-12-27 02:22:37 +0300

    set up the fetch tours function to fetch data from the api

--- src/App.js
+++ src/App.js
@@ -5,10 +5,34 @@ import Tours from "./Tours";
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // Fetch data from the api
+  const fetchTours = async () => {
+    setLoading(true);
+    const resp = await fetch(url);
+    const tours = await resp.json();
+    console.log(tours);
+  };
+
+  // useEffect hook to render the tours
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      <h2>Tours app</h2>
-    </div>
+    <main>
+      <Tours />
+    </main>
   );
 }
 
