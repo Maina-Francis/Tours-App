8d5869dec16fdb2d324e2e3a07c16349cfc42dab
Francis Maina <sebastianfrancix@gmail.com>
2022-12-27 02:49:44 +0300

    rendered info from our api in the Tour component

--- src/Tour.js
+++ src/Tour.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 
 const Tour = (props) => {
-  console.log(props);
-  return <h2>Tour Component</h2>;
+  //   console.log(props);
+  const { id, name, info, image, price } = props;
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{info}</p>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
--- src/Tours.js
+++ src/Tours.js
@@ -2,7 +2,7 @@ import React from "react";
 import Tour from "./Tour";
 
 const Tours = ({ tours }) => {
-  console.log(tours);
+  //   console.log(tours);
 
   return (
     <section>
